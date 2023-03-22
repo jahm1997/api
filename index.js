aa9661dcb2bd2b4ed813ec886830dcce4f68780c
jahm1997 <jahm1997@gmail.com>
2023-03-22 18:47:16 -0500

    10mo commit

--- index.js
+++ index.js
@@ -20,11 +20,11 @@
 require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+// const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  const port = process.env.PORT || 3001;
-  server.listen(port, () => {
+  server.listen(3001, () => {
     console.log("%s listening at " + port); // eslint-disable-line no-console
   });
 });
--- src/db.js
+++ src/db.js
@@ -4,20 +4,20 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
-//   // DB_DEPLOY,
-//   {
-//     logging: false, // set to console.log to see the raw SQL queries
-//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//   }
-// );
+const sequelize = new Sequelize(
+  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+  DB_DEPLOY,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
+);
 
-const sequelize = new Sequelize("dogs_su0k", DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: "postgres",
-  logging: false,
-});
+// const sequelize = new Sequelize("dogs_su0k", DB_USER, DB_PASSWORD, {
+//   host: DB_HOST,
+//   dialect: "postgres",
+//   logging: false,
+// });
 
 const basename = path.basename(__filename);
 
